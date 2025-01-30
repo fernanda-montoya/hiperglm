9f0a5fd8c393d36cb92329839918f18452d4f6e7
fernanda-montoya <fmontoy1@jh.edu>
2025-01-29 20:24:05 -0500

    implement functions and roxygenize

---
+++ R/hiper_glm.R
@@ -0,0 +1,8 @@
+#' @export
+hiper_glm <- function(design, outcome) {
+  #TODO Implement model fitting
+  warning("This function is yet to be implmented.")
+  hglm_out <- list()
+  class(hglm_out) <- "hglm"
+  return(hglm_out)
+}
---
+++ R/model_output.R
@@ -0,0 +1,14 @@
+#' @export
+coef.hglm <- function(hglm_out) {
+  warning("This function is yet to be implemented.")
+}
+
+#' @export
+vcov.hglm <- function(hglm_out) {
+  warning("This function is yet to be implemented.")
+}
+
+#' @export
+print.hglm <- function(hglm_out) {
+  cat("Output of hglm_out.\n")
+}
